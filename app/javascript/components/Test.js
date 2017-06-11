4fd91cc3cd38ee12ea9048a06dafe8bc24c3c954
Andrej Antas <andrej@antas.cz>
2017-06-11 22:05:06 +0200

    Dummy component

---
+++ app/javascript/components/Test.js
@@ -0,0 +1,15 @@
+import React, { Component  } from 'react';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h3>Somewhat famous quotes here!</h3>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
